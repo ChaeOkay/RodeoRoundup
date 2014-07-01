9bd3f0e99093648cfea431dc0d7dfc58736505fb
Chae O'Keefe <chaemobile@gmail.com>
2014-07-01 15:28:10 -0400

    Update seedfile to create members and organizations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+member_1 = { username: 'member1',
+             first_name: 'Beatrix',
+             last_name: 'Kiddo',
+            }
+
+member_2 = { username: 'member2',
+             first_name: 'Felix',
+             last_name: 'TheHousecat',
+           }
+
+organization_1 = { name: 'ATL',
+                   address_1: '1 Peachtree',
+                   city: 'Atlanta',
+                   state: 'GA',
+                   zip: '30309',
+                   phone: '000-000-00000',
+                 }
+
+organization_2 = { name: 'NYC',
+                   address_1: '100 Broadway',
+                   city: 'New York',
+                   state: 'NY',
+                   zip: '10007',
+                   phone: '111-111-1111',
+                 }
+
+organization_3 = { name: 'MI',
+                   address_1: '1010 Saginaw',
+                   city: 'Lansing',
+                   state: 'MI',
+                   zip: '48917',
+                   phone: '222-222-2222',
+                 }
+
+ActiveRecord::Base.transaction do
+  m1 = Member.create(member_1)
+  m1.organizations.create(organization_1)
+  m1.organizations.create(organization_2)
+
+  m2 = Member.create(member_2)
+  m2.organizations.create(organization_3)
+end
+
