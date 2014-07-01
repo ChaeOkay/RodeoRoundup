6016ee98d78534e2e53cb8506a9909f761cc9513
Chae O'Keefe <chaemobile@gmail.com>
2014-07-01 14:52:38 -0400

    Standardize format to match organizations, DRY up

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -6,27 +6,29 @@ describe MembersController do
 
     let!(:member) { FactoryGirl.create(:member) }
 
-    before do
-      get :show, id: member.id, format: :json
-    end
-
     it 'returns 200 on success' do
+      get :show, id: member.id, format: :json
       expect(response.code).to eq '200'
     end
 
-    it 'includes the member id' do
-      json_response = JSON.parse(response.body)
-      expect(json_response['member']['id']).to eq member.id
-    end
+    describe 'attributes' do
 
-    it 'includes the member first name' do
-      json_response = JSON.parse(response.body)
-      expect(json_response['member']['first_name']).to eq member.first_name
-    end
+      before do
+        get :show, id: member.id, format: :json
+        @response = JSON.parse(response.body)
+      end
 
-    it 'includes the member last name' do
-      json_response = JSON.parse(response.body)
-      expect(json_response['member']['last_name']).to eq member.last_name
+      it 'includes the member id' do
+        expect(response['member']['id']).to eq member.id
+      end
+
+      it 'includes the member first name' do
+        expect(response['member']['first_name']).to eq member.first_name
+      end
+
+      it 'includes the member last name' do
+        expect(response['member']['last_name']).to eq member.last_name
+      end
     end
   end
 
