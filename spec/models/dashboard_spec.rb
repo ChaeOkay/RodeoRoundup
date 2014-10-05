6c7092b16997bc9979dbda2bcdcf1feab6b33f74
Chae <chaemobile@gmail.com>
2014-10-05 19:36:37 -0400

    Cleans up spec
    * extracts params into variable
    * removes mysterious numbers

--- spec/models/dashboard_spec.rb
+++ spec/models/dashboard_spec.rb
@@ -5,21 +5,22 @@ describe Dashboard do
     let!(:member) { FactoryGirl.create(:member) }
     let!(:organization) { FactoryGirl.create_list(:organization, 3) }
 
-    let(:dashboard) { described_class.new(resources: ['members', 'organizations']) }
+    let(:resources) { ['members', 'organizations'] }
+    let(:dashboard) { described_class.new(resources: resources) }
     subject(:serialized_dashboard) { dashboard.serialized }
 
     it 'includes each dashboard as the key' do
-      expect(serialized_dashboard.size).to eq 2
+      expect(serialized_dashboard.size).to eq resources.size
     end
 
     it 'includes all members in the response' do
       members_serializer = serialized_dashboard['members']
-      expect(members_serializer.object.size).to eq 4
+      expect(members_serializer.object.size).to eq Member.all.size
     end
 
     it 'includes all organizations in the response' do
       organizations_serializer = serialized_dashboard['organizations']
-      expect(organizations_serializer.object.size).to eq 3
+      expect(organizations_serializer.object.size).to eq Organization.all.size
     end
   end
 end
